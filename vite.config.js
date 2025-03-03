c3220635cfd7f7c5a95b440c2a64701f501dff56
Muhammad Zain Haseeb <142337757+Maverick474@users.noreply.github.com>
2025-03-03 22:25:57 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [
-    base: '/Portfolio/',
     react(),
     tailwindcss(),
   ],
