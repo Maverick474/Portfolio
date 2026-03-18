dad18557bdf151141f488bc908621ccc31023b3e
Muhammad Zain Haseeb <142337757+Maverick474@users.noreply.github.com>
2026-03-18 10:58:31 +0500

    Revise About section with updated biography
    Updated personal description with professional achievements and graduation details.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,12 @@ const About = () => {
       <div className='flex items-center justify-between py-20'>
         <div>
             <h1 className='md:text-3xl md:font-bold text-2xl font-semibold md:mt-3 fade-text uppercase'><span style={{color: '#00ABF1'}}>Muhammad</span> Zain Haseeb</h1>
-            <p className='text-justify mr-6 mt-2 fade-text'>I am an enthusiastic and detail-oriented individual with a passion for technology and 
-                problem-solving. 
-                With strong analytical skills and a commitment to meeting deadlines,
-                I excel at tacklingcomplex challenges. 
-                I enjoy exploring new fields, particularly in web development and emergingtechnologies 
-                such as AI/ML. 
-                Currently, I am in my 8th semester at Forman Christian College, pursuing Bachelor's 
-                in Computer Science.</p>
+            <p className='text-justify mr-6 mt-2 fade-text'>I am an enthusiastic and detail-oriented professional with a strong passion for problem-solving.
+            With solid analytical abilities and a proven track record of meeting deadlines, I thrive in tackling
+            complex challenges. I enjoy exploring new domains, particularly in web development and emerging
+            technologies such as Machine learning and Generative AI. I was honored with the Best Research
+            FYP Award for my final year project and have recently graduated from Forman Christian College
+            University.</p>
 
             <a href="https://www.linkedin.com/in/muhammad-zain-haseeb-aa76381bb" target='_blank' rel="noopener noreferrer">
               <button className='mt-6 p-4 font-semibold custom-btn fade-text'>Connect With Me</button>
